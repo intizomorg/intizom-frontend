c2541ff0d5d6d0ca0998ee4cf6064b04b3140364
intizom1 <uzbekuzbekistan2025@gmail.com>
2026-01-05 11:10:44 -0800

    fix: repair tailwind postcss config and restore build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["intizom.org", "api.intizom.org"],
+  },
+};
+
+module.exports = nextConfig;
